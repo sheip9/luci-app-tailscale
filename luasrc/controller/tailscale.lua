ceb457e7889c1832409d3cf6c97143160cb0dcd9
sheip9 <53911290+sheip9@users.noreply.github.com>
2024-10-22 13:55:36 +0800

    Update tailscale.lua

--- luasrc/controller/tailscale.lua
+++ luasrc/controller/tailscale.lua
@@ -1,19 +1,31 @@
+
 module("luci.controller.tailscale", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/tailscale") then
-		return
-	end
+        if not nixio.fs.access("/etc/config/tailscale") then
+                return
+        end
 
 entry({"admin","vpn"}, firstchild(), "VPN", 45).dependent = false
 entry({"admin", "vpn", "tailscale"}, alias("admin", "vpn", "tailscale", "base"), _("Tailscale"), 99)
 entry({"admin", "vpn", "tailscale", "base"}, cbi("tailscale/base"), _("Base Setting"), 1)
 entry({"admin", "vpn", "tailscale", "status"}, call("act_status"))
+entry({"admin", "vpn", "tailscale", "status_detail"}, call("status_detail"))
 end
 
 function act_status()
-local e={}
-  e.running=luci.sys.call("pgrep /usr/bin/tailscaled >/dev/null")==0
-  luci.http.prepare_content("application/json")
-  luci.http.write_json(e)
+        local e = {}
+        e.running = luci.sys.call("pgrep /usr/sbin/tailscaled >/dev/null") == 0
+        luci.http.prepare_content("application/json")
+        luci.http.write_json(e)
+end
+
+function status_detail()
+        local json = require "luci.jsonc"
+        local sys = require "luci.sys"
+        local text = sys.exec("tailscale status --json")
+        luci.http.prepare_content("application/json")
+        local m = json.parse(text)
+        luci.http.write_json(m)
+
 end
