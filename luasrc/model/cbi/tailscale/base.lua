34fae8269d97f8a84fa9add70392a403bf3caaaf
sheip9 <53911290+sheip9@users.noreply.github.com>
2024-10-22 17:27:22 +0800

    Update base.lua

--- luasrc/model/cbi/tailscale/base.lua
+++ luasrc/model/cbi/tailscale/base.lua
@@ -1,50 +1,77 @@
-a = Map("tailscale")                                                                                                                                                                                                                            
-a.title = translate("Tailscale")                                                                                                                                                                                                                
-a.description = translate("Tailscale connects your team's devices and development environments for easy access to remote resources.")                                                                                                           
-                                                                                                                                                                                                                                                
-a:section(SimpleSection).template  = "tailscale/tailscale_status"                                                                                                                                                                               
-                                                                                                                                                                                                                                                
-t = a:section(NamedSection, "settings", "status")                                                                                                                                                                                               
-t.anonymous = true                                                                                                                                                                                                                              
-t.addremove = false                                                                                                                                                                                                                             
-                                                                                                                                                                                                                                                
-e = t:option(Flag, "enabled", translate("Enable"))                                                                                                                                                                                              
-e.default = 0                                                                                                                                                                                                                                   
-e.rmempty = false                                                                                                                                                                                                                               
-                                                                                                                                                                                                                                                
-e = t:option(Button, "Status", translate("Stauts"))                                                                                                                                                                                             
-e.rawhtml = true                                                                                                                                                                                                                                
-e.template = "tailscale/status_detail"                                                                                                                                                                                                          
-                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                
-adv = a:section(NamedSection, "settings", "Advance")                                                                                                                                                                                            
-                                                                                                                                                                                                                                                
-e = adv:option(Value, "port", translate("Port"))                                                                                                                                                                                                
-e.rmempty = true                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                
-e = adv:option(Value, "subnet_routes", translate("Advertise Routes"))                                                                                                                                                                           
-e.placeholder = "192.168.31.0/24"                                                                                                                                                                                                               
-e.rmempty = true                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                
-e = adv:option(Value, "hostName", translate("Hostname"))                                                                                                                                                                                        
-e.placeholder = ""                                                                                                                                                                                                                              
-e.rmempty = true                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                
-e = adv:option(Value, "custom", translate("Custom Option"))                                                                                                                                                                                     
-e.placeholder = ""                                                                                                                                                                                                                              
-e.rmempty = true                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                
--- e = t:option(DummyValue, "opennewwindow", translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"tailscale.com\" onclick=\"window.open('https://login.tailscale.com/admin/machines')\" />"))                        
--- e.description = translate("Create or manage your tailscale network")                                                                                                                                                                         
-                                                                                                                                                                                                                                                
--- e = t:option(DummyValue, "_authinfo", translate("Log"))                                                                                                                                                                                      
-                                                                                                                                                                                                                                                
--- function e.cfgvalue(self, section)                                                                                                                                                                                                           
---    local logfile = "/tmp/tailscale.log"                                                                                                                                                                                                      
---    local content = nixio.fs.readfile(logfile)                                                                                                                                                                                                
---    return content or ""                                                                                                                                                                                                                      
---  end                                                                                                                                                                                                                                         
-                                                                                                                                                                                                                                                
+a = Map("tailscale")
+a.title = translate("Tailscale")
+a.description = translate("Tailscale connects your team's devices and development environments for easy access to remote resources.") 
+
+a:section(SimpleSection).template  = "tailscale/tailscale_status"    
+
+t = a:section(TypedSection, "tailscale", "status")
+t.anonymous = true
+t.addremove = false
+
+
+t:tab("settings", translate("Basic Settings"))
+
+
+e = t:taboption("settings", Flag, "enabled", translate("Enable"))
+e.default = 0
+e.rmempty = false
+
+e = t:taboption("settings", Button, "Status", translate("Stauts"))
+e.rawhtml = true
+e.template = "tailscale/status_detail"
+
+o = t:taboption("settings", Value, 'port', "Port", "Set the Tailscale port number.")
+o.datatype = 'port'
+o.default = '41641'
+o.rmempty = false
+
+o = t:taboption("settings", Value, 'config_path', "Workdir", "The working directory contains config files, audit logs, and runtime info.")
+o.default = '/etc/tailscale'
+o.rmempty = false
+
+o = t:taboption("settings", ListValue, 'fw_mode', "Firewall Mode")
+o:value("nftables", "nftables")
+o:value("iptables", "iptables")
+o.default = 'nftables'
+o.rmempty = false
+
+
+t:tab("advance", translate("Advanced Settings"));
+
+o = t:taboption("advance", Flag, "acceptRoutes", translate("Accept Routes"), translate("Accept subnet routes that other nodes advertise."));
+o.default = o.disabled;
+o.rmempty = false;
+
+o = t:taboption("advance", Value, "hostname", translate("Device Name"), translate("Leave blank to use the device's hostname."));
+o.default = "";
+o.rmempty = true;
+
+o = t:taboption("advance", Flag, "acceptDNS", translate("Accept DNS"), translate("Accept DNS configuration from the Tailscale admin console."));
+o.default = o.enabled;
+o.rmempty = false;
+
+o = t:taboption("advance", Flag, "advertiseExitNode", translate("Exit Node"), translate("Offer to be an exit node for outbound internet traffic from the Tailscale network."));
+o.default = o.disabled;
+o.rmempty = false;
+
+t:tab("extra", translate("Extra Settings"));
+
+o = t:taboption("extra", DynamicList, "flags", translate("Additional Flags"), translate("List of extra flagt: Format: --flags=value, e.g. <code>--exit-node=10.0.0.1</code>. <br> <a href='https://tailscale.com/kb/1241/tailscale-up' target='translateblank'>Available flags</a> for enabling settings upon the initiation of Tailscale."));
+o.default = "";
+o.rmempty = true;
+
+
+s = a:section(NamedSection, "settings", "config");
+s.title = translate("Custom Server Settings");
+s.description = translate("Use <a href='https://github.com/juanfont/headscale' target='translateblank'>headscale</a> to deploy a private server.");
+
+o = s:option(Value, "loginServer", translate("Server Address"));
+o.default = "";
+o.rmempty = true;
+
+o = s:option(Value, "authKey", translate("Auth Key"));
+o.default = "";
+o.rmempty = true;
+
+
 return a    
